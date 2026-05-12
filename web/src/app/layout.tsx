b343f059e55db3ecb61b718844df201fd10fc01d
babychucks <u24730841@tuks.co.za>
2026-05-12 21:02:56 +0200

    Adding missed changes from shared citizen-sidebar component start implementation

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-import { AppSidebar } from '@/components/organisms/app-sidebar'
+import { AppSidebar } from '@/components/organisms/citizen-sidebar'
 
 const inter = Inter({
   variable: '--font-inter',
