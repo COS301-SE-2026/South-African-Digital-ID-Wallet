e1f18c8845accae0691233a1b39115c3246e900f
babychucks <u24730841@tuks.co.za>
2026-05-12 21:23:06 +0200

    Added button and header to citizen-sidebar

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-import { AppSidebar } from '@/components/organisms/citizen-sidebar'
+import { CitizenSidebar } from '@/components/organisms/citizen-sidebar'
 
 const inter = Inter({
   variable: '--font-inter',
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en" className={inter.variable}>
       <body>
         <div className="flex min-h-screen bg-background">
-          <AppSidebar />
+          <CitizenSidebar />
 
           <div className="min-h-screen flex-1">
             <main>{children}</main>
--- web/src/components/organisms/citizen-sidebar.tsx
+++ web/src/components/organisms/citizen-sidebar.tsx
@@ -1,5 +1,8 @@
+'use client'
 import {
   Bell,
+  ChevronLeft,
+  ChevronRight,
   History,
   LayoutDashboard,
   LockKeyhole,
@@ -8,8 +11,11 @@ import {
   ShieldCheck,
   WalletCards,
 } from 'lucide-react'
+
 import Image from 'next/image'
 import FlashIdWhite from '@/assets/images/FlashID-white.png'
+
+import { useState } from 'react'
 
 const citizenNav = [
   { label: 'Dashboard', icon: LayoutDashboard },
@@ -24,6 +30,47 @@ const securityNav = [
   { label: 'Privacy Settings', icon: Settings },
 ]
 
-export function AppSidebar() {
-  return <div></div>
+export function CitizenSidebar() {
+  const [isCollapsed, setIsCollapsed] = useState(false)
+
+  return (
+    <aside
+      className={
+        'hidden min-h-screen flec-col bg-deep-green px-4 py-5 text-clean-white transition-all duration-300 lg:flex ${ isCollapsed? "w-24": "w-64"}'
+      }
+    >
+      <div
+        className={
+          'mb-8 flex items-center ${isCollapsed ? "justify-center" : "justify-between"}'
+        }
+      >
+        <div className="flex items-center gap-3">
+          <div className="flex h-12 w-12 items-center justify-center overflow-hidden rounded-full">
+            <Image
+              src={FlashIdWhite}
+              alt="Flash ID logo"
+              width={48}
+              height={48}
+              className="h-12 w-12 object-contain"
+              priority
+            />
+          </div>
+          {!isCollapsed && (
+            <p className="text-xl font-bold whitespace-nowrap">Flash ID</p>
+          )}
+
+          {!isCollapsed && (
+            <button
+              type="button"
+              onClick={() => setIsCollapsed(true)}
+              className="rounded-xl p-2 text-clean-white/70 transition hover:bg-clean-white/10 hover:text-clean-white"
+              aria-label="Collapse sidebar"
+            >
+              <ChevronLeft className="h-5 w-5" />
+            </button>
+          )}
+        </div>
+      </div>
+    </aside>
+  )
 }
