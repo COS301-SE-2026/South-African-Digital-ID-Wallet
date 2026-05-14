fb6d0c90864ffe2f37713b5bafbb86f6e9bf5682
Dominiqu <u24580482@tuks.co.za>
2026-05-14 14:04:31 +0200

    implementation login card and exports

---
+++ web/src/components/organisms/login-card.tsx
@@ -0,0 +1,9 @@
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Subtitle, Title } from '@/components/atoms'
+import { LoginForm } from '@/components/molecules'
+
+type LoginCardProps = {
+  className?: string
+  title?: string
+  subtitle?: string
+}
