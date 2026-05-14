426a8c421deb745fc1481d97fe835d65630c9b62
Dominiqu <u24580482@tuks.co.za>
2026-05-14 19:29:20 +0200

    login-page routing

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,15 +1,5 @@
-import { AuthSidebar, LoginCard } from '@/components/organisms'
+import { LoginPage } from '@/components/pages'
 
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-cream-background text-deep-green">
-      <div className="flex min-h-screen flex-col lg:flex-row">
-        <AuthSidebar />
-
-        <div className="flex flex-1 items-center justify-center px-6 py-10 sm:px-8 lg:px-10 xl:px-14">
-          <LoginCard className="w-full max-w-xl min-h-136 lg:min-h-152" />
-        </div>
-      </div>
-    </main>
-  )
+  return <LoginPage />
 }
