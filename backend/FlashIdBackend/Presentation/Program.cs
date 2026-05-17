baee9be599c08ba916c692b22f7bc348c5934e19
infamouszay <u22506099@tuks.co.za>
2026-05-17 22:14:46 +0200

    Update Program.cs to remove weather forecast placeholder and wire up controllers

--- backend/FlashIdBackend/Presentation/Program.cs
+++ backend/FlashIdBackend/Presentation/Program.cs
@@ -5,7 +5,6 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -22,12 +21,6 @@ using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     await DbSeeder.SeedAsync(db);
-}
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
