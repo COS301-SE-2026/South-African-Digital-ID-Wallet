3f0a4f0c8bd3407efe2e990749345cb8b8dda220
TINODIWA <nathaniel.chisadza@gmail.com>
2026-05-19 02:23:16 +0200

    Registration UI types

---
+++ web/src/types/registration-form.types.ts
@@ -0,0 +1,8 @@
+export type RegistrationCredentials = {
+  idnumber: string
+  username: string
+  password: string
+}
+export type RegistrationFormProps = {
+  onSubmitAction?: (data: RegistrationCredentials) => void
+}
