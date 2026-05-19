4e09cf8b44e8b515642aa5a169fe86238fb9a3e9
TINODIWA <nathaniel.chisadza@gmail.com>
2026-05-19 06:29:44 +0200

    Adde components together for the full Registration page

---
+++ web/src/app/registration/page.tsx
@@ -0,0 +1,5 @@
+import { RegistrationPage } from '@/components/pages'
+
+export default function Registration() {
+  return <RegistrationPage />
+}
---
+++ web/src/types/registration-card.types.ts
@@ -0,0 +1,5 @@
+﻿export type RegistrationCardProps = {
+  className?: string
+  title?: string
+  subtitle?: string
+}
