06cfff853538595ebab5d40cf66d0df8e905b8ff
TINODIWA <nathaniel.chisadza@gmail.com>
2026-05-20 03:47:43 +0200

    Setting up Verification Method Selection

--- web/src/types/registration-form.types.ts
+++ web/src/types/registration-form.types.ts
@@ -1,8 +1,13 @@
+export type VerificationMethod = 'activation' | 'physical'
+
 export type RegistrationCredentials = {
   idnumber: string
   username: string
   password: string
+  activationCode: string
+  verificationMethod: VerificationMethod
 }
+
 export type RegistrationFormProps = {
   onSubmitAction?: (data: RegistrationCredentials) => void
 }
