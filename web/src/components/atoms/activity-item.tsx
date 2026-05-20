caa54d483188e8d0b29ba493ceabdb3df00e8812
Dominiqu <u24580482@tuks.co.za>
2026-05-20 08:04:17 +0200

    activity item atom implementation

---
+++ web/src/components/atoms/activity-item.tsx
@@ -0,0 +1,22 @@
+type ActivityItemProps = {
+  title: string
+  subtitle?: string
+  time?: string
+}
+
+export const ActivityItem = ({
+  title,
+  subtitle,
+  time,
+}: Readonly<ActivityItemProps>) => {
+  return (
+    <div className="border rounded-2xl p-5 flex justify-between items-center">
+      <div>
+        <p className="font-semibold">{title}</p>
+        {subtitle && <p className="text-muted-text text-sm">{subtitle}</p>}
+      </div>
+
+      {time && <span className="text-muted-text text-sm">{time}</span>}
+    </div>
+  )
+}
