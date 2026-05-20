7200c8d60c9292c7753ca22c3ca15962ff2268ee
babychucks <babychucks05@gmail.com>
2026-05-21 00:02:09 +0200

    Updated mock-gov Dto and Added Mock citizens to Mock Registry service

--- backend/FlashIdBackend/Application/Features/Onboarding/Dtos/MockIdentityRecordDto.cs
+++ backend/FlashIdBackend/Application/Features/Onboarding/Dtos/MockIdentityRecordDto.cs
@@ -5,7 +5,13 @@ public class MockIdentityRecordDto
     public string? SaId { get; set; } = string.Empty;
     public string Names { get; set; } = string.Empty;
     public string Surname { get; set; } = string.Empty;
+    public string Fullname => $"{Names} {Surname}";
     public DateTime DateOfBirth { get; set; }
     public string Status { get; set; } = string.Empty;
+
+    public string? Gender { get; set; }
+    public string? Nationality { get; set; }
+
+    public bool IsActive => Status == "Active";
 }
 
---
+++ backend/FlashIdBackend/Infrastructure/Services/MockGovernmentRegistryService.cs
@@ -0,0 +1,50 @@
+using Application.Features.Onboarding.Dtos;
+
+namespace Infrastructure.Services;
+
+public class MockGovernmentRegistryService
+{
+    private static readonly List<MockIdentityRecordDto> MockRecords =
+    [
+        new()
+        {
+            SaId = "0000001971025",
+            Names = "Tiana",
+            Surname = "Rogers",
+            DateOfBirth = new DateTime(1997, 10, 25),
+            Gender = "Female",
+            Nationality = "South African",
+            Status = "Active"
+        },
+        new()
+        {
+            SaId = "0000001960615",
+            Names = "Naveen",
+            Surname = "Rogers",
+            DateOfBirth = new DateTime(1996, 6, 15),
+            Gender = "Male",
+            Nationality = "South African",
+            Status = "Active"
+        },
+        new()
+        {
+            SaId = "0000002010302",
+            Names = "Po",
+            Surname = "Ping",
+            DateOfBirth = new DateTime(2001, 3, 2),
+            Gender = "Male",
+            Nationality = "South African",
+            Status = "Inactive"
+        },
+        new()
+        {
+            SaId = "0000002740723",
+            Names = "Po",
+            Surname = "Ping",
+            DateOfBirth = new DateTime(1974, 07, 23),
+            Gender = "Male",
+            Nationality = "South African",
+            Status = "Active"
+        }
+    ];
+}
