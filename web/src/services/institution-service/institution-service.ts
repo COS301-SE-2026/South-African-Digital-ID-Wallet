188ada894052bb435850f41865cc3a39e11ef49a
infamouszay <u22506099@tuks.co.za>
2026-05-20 22:25:14 +0200

    Add institution service, fix urls export, add index barrel

---
+++ web/src/services/institution-service/institution-service.ts
@@ -0,0 +1,14 @@
+import axios, { AxiosResponse } from 'axios'
+import institutionUrls from './institution-urls'
+import { registerInstitutionDto } from './institution-dto'
+import { RegisterInstitutionFormValues } from './types'
+
+const register = (formData: RegisterInstitutionFormValues) => {
+  const url = institutionUrls.register()
+  const dto = registerInstitutionDto(formData)
+  return axios.post(url, dto).then((res: AxiosResponse) => res.data)
+}
+
+const institutionService = { register }
+
+export default institutionService
--- web/src/services/institution-service/institution-urls.ts
+++ web/src/services/institution-service/institution-urls.ts
@@ -1,7 +1,9 @@
 const apiUrl = process.env.NEXT_PUBLIC_API_URL
 
-export default {
+const institutionUrls = {
   register: (): string => `${apiUrl}/api/institutions/register`,
   getAll: (): string => `${apiUrl}/api/institutions`,
   getById: (id: string): string => `${apiUrl}/api/institutions/${id}`,
 }
+
+export default institutionUrls
