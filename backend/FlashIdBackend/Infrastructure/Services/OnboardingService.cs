91403bc4ef223ed47fc35d2251bf6607e0204e61
babychucks <babychucks05@gmail.com>
2026-05-21 04:18:52 +0200

    Adding db communication to insert a new entry in user and citizen tables

--- backend/FlashIdBackend/Infrastructure/Services/OnboardingService.cs
+++ backend/FlashIdBackend/Infrastructure/Services/OnboardingService.cs
@@ -1,17 +1,20 @@
 using Application.Features.Onboarding.Dtos;
 using Application.Features.Onboarding.Exceptions;
 using Application.Common.Interfaces;
-
+using Domain.Entities;
+using Domain.Enums;
+using Infrastructure.Data;
 
 namespace Infrastructure.Services;
 
 public class OnboardingService : IOnboardingService
 {
     private readonly MockGovernmentRegistryService _registryService;
-
-    public OnboardingService(MockGovernmentRegistryService registryService)
+    private readonly AppDbContext _context;
+    public OnboardingService(MockGovernmentRegistryService registryService, AppDbContext context)
     {
         _registryService = registryService;
+        _context = context;
     }
 
     public OnboardCitizenResponse OnboardCitizen(OnboardCitizenRequest request)
@@ -27,6 +30,29 @@ public class OnboardingService : IOnboardingService
         {
             throw new IdentityRecordNotFoundException();
         }
+
+        var existingCitizen = _context.Citizens
+            .FirstOrDefault(citizen => citizen.SaId == request.SaId);
+
+        if (existingCitizen is not null)
+        {
+            throw new Exception("Citizen already exists in FlashID.");
+        }
+
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            Names = identityRecord.Names,
+            Surname = identityRecord.Surname,
+            Email = request.Email,
+            PhoneNumber = request.PhoneNumber,
+            Username = request.Email,
+            Role = UserRole.Citizen,
+            IsEmailVerified = false,
+            IsDeleted = false,
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow
+        };
 
         var activationCode = Random.Shared.Next(100000, 999999).ToString();
 
