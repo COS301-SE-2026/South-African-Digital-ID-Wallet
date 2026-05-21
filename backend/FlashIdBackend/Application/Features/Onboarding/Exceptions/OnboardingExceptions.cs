942e872931db356a47aa8bf209305699c2520c62
babychucks <babychucks05@gmail.com>
2026-05-21 03:04:31 +0200

    Added Citizen consent exception

--- backend/FlashIdBackend/Application/Features/Onboarding/Exceptions/OnboardingExceptions.cs
+++ backend/FlashIdBackend/Application/Features/Onboarding/Exceptions/OnboardingExceptions.cs
@@ -7,3 +7,11 @@ public class IdentityRecordNotFoundException : Exception
     {
     }
 }
+
+public class CitizenConsentRequiredException : Exception
+{
+    public CitizenConsentRequiredException()
+        : base("Citizen consent is required before onboarding.")
+    {
+    }
+}
