9bdc76531626d93e7bb475d173fd5cb02ef6df88
TINODIWA <nathaniel.chisadza@gmail.com>
2026-05-30 01:12:59 +0200

    Removed AutoMapper, Added Mapperly & created CitizenMapper.cs

---
+++ backend/FlashIdBackend/Application/Mappers/CitizenMapper.cs
@@ -0,0 +1,17 @@
+using Application.Features.Citizens.DTOs;
+using Domain.Entities;
+using Riok.Mapperly.Abstractions;
+
+[Mapper]
+public partial class CitizenMapper
+{
+    // written manually since registration response combines fields from 2 entities, meaning Mapperly will not generate this
+    public RegisterCitizenResponseDto ToDto(Citizen citizen, User user) => new()
+    {
+        CitizenId = citizen.Id,
+        UserId = user.Id,
+        SaId = citizen.SaId,
+        Username = user.Username,
+        CreatedAt = citizen.CreatedAt,
+    };
+}
