bec10defd92c7332ce7f1195bca07afa23df2a41
TINODIWA <nathaniel.chisadza@gmail.com>
2026-06-08 15:43:19 +0200

    Updated Dependency injections in application and infrastructure layer

--- backend/FlashIdBackend/Application/DependencyInjection.cs
+++ backend/FlashIdBackend/Application/DependencyInjection.cs
@@ -1,6 +1,8 @@
 using Application.Common.Interfaces;
+using Application.Common.Interfaces.ServiceInterfaces;
 using Application.Common.Mapping;
 using Application.Common.Services;
+using Infrastructure.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application;
@@ -10,8 +12,10 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddScoped<ICitizenService, CitizenService>();
+        services.AddScoped<IInstitutionService, InstitutionService>();
 
         services.AddSingleton<CitizenMapper>();
+        services.AddSingleton<InstitutionMapper>();
 
         return services;
     }
