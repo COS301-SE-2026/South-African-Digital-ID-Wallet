42e4837ee75dc71a3624913c3f9413b7788e1ecc
TINODIWA <nathaniel.chisadza@gmail.com>
2026-06-25 08:27:30 +0200

    Added index & service to services folder for integration

--- web/src/services/citizen-register-service/register-service.ts
+++ web/src/services/citizen-register-service/register-service.ts
@@ -1,1 +1,15 @@
 import axios, { AxiosResponse } from 'axios'
+
+import registerUrls from './register-urls'
+import { registerDto } from './register-dto'
+import { RegisterFormValues } from './types'
+
+const register = (formData: RegisterFormValues) => {
+  const url = registerUrls.citizenRegistration()
+  const dto = registerDto(formData)
+  return axios.post(url, dto).then((res: AxiosResponse) => res.data)
+}
+
+export default {
+  register,
+}
