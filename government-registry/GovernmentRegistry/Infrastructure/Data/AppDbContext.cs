a411e2cdcfec1b1f311a377a4a31c237449b2476
babychucks <babychucks05@gmail.com>
2026-06-29 20:21:06 +0200

    AppDbContext added

---
+++ government-registry/GovernmentRegistry/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<CitizenRecord> CitizenRecords { get; set; }
+    public DbSet<Credential> Credentials { get; set; }
+    public DbSet<IdentityDocument> IdentityDocuments { get; set; }
+    public DbSet<DriversLicense> DriversLicenses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
