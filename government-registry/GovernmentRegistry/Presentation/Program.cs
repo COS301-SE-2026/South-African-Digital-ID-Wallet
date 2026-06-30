10edf34087cb4f702b23be7013242da2bd8f1ed5
babychucks <babychucks05@gmail.com>
2026-06-30 13:32:19 +0200

    Configured dependency injections and project references

--- government-registry/GovernmentRegistry/Presentation/Program.cs
+++ government-registry/GovernmentRegistry/Presentation/Program.cs
@@ -1,8 +1,11 @@
+using Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
@@ -39,3 +42,5 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
+
