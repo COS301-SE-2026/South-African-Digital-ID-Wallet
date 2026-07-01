55f15cf854c0d462026709b60ee23d1e1bba219b
Dominiqu <u24580482@tuks.co.za>
2026-07-01 18:31:11 +0200

    citizen dashboard portal page.tsx

---
+++ web/src/app/(portal)/citizen/citizen-dashboard/page.tsx
@@ -0,0 +1,7 @@
+// import { ViewInstitutionsPage } from '@/components/pages/gov-admin/view-institutions-page'
+import { UnderConstructionPage } from '@/components/pages/under-construction-page'
+
+export default function Page() {
+  //   return <ViewInstitutionsPage />
+  return <UnderConstructionPage />
+}
