19b096e795dec945ba981e4480090116bd808d94
babychucks <babychucks05@gmail.com>
2026-07-02 02:34:26 +0200

    Fixed bugs :) Gov Registry implemented with working API !!!

--- government-registry/GovernmentRegistry/Infrastructure/DependencyInjection.cs
+++ government-registry/GovernmentRegistry/Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
+using Application.Common.Interfaces.RepositoryInterfaces;
 using Infrastructure.Data;
+using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +12,7 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
        services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+       services.AddScoped<ICitizenRecordRepository, CitizenRecordRepository>();
        return services;
     }
 }
--- government-registry/GovernmentRegistry/Presentation/Program.cs
+++ government-registry/GovernmentRegistry/Presentation/Program.cs
@@ -12,13 +12,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 
 var app = builder.Build();
-
-app.UseMiddleware<ApiKeyMiddleware>();
-app.MapControllers();
 
 using (var scope = app.Services.CreateScope())
 {
@@ -32,6 +32,12 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -39,6 +45,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseMiddleware<ApiKeyMiddleware>();
+app.MapControllers();
+
+
 app.Run();
 
 
