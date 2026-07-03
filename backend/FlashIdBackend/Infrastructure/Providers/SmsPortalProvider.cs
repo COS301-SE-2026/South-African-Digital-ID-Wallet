69e4b0a1b0b9c65c8efa10987810901ad6779048
babychucks <babychucks05@gmail.com>
2026-07-03 20:22:54 +0200

    Updated SmsPortalProvider to make request to SMS Portal.

--- backend/FlashIdBackend/Infrastructure/Providers/SmsPortalProvider.cs
+++ backend/FlashIdBackend/Infrastructure/Providers/SmsPortalProvider.cs
@@ -7,22 +7,42 @@ namespace Infrastructure.Providers;
 
 public class SmsPortalProvider : ISmsProvider
 {
-    private readonly HttpClient _client;
-    private readonly IServiceProvider _serviceProvider;
+    private readonly HttpClient _httpClient;
 
-    public SmsPortalProvider(HttpClient client, IServiceProvider serviceProvider)
+    public SmsPortalProvider(HttpClient client)
     {
-        _client = client;
-        _serviceProvider = serviceProvider;
+        _httpClient = client;
     }
     public async Task SendSmsAsync(string number, string message)
     {
-        var configuration = _serviceProvider.GetRequiredService<IConfiguration>();
-        var apiKey = configuration["SmsPortalProvider:ApiKey"];
-        var apiSecret = configuration["SmsPortalProvider:ApiSecret"];
+        var jsonSendRequest = $$"""
+                                {
+                                "messages": [
+                                {
+                                "content":"{{message}}",
+                                "destination": "{{number}}",
+                                }
+                                ]
+                                }
+                                """;
 
-        var apiCredential = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{apiKey}:{apiSecret}"));
+        using var request = new HttpRequestMessage(HttpMethod.Post, "BulkMessages")
+        {
+            Content = new StringContent(
+                jsonSendRequest,
+                Encoding.UTF8,
+                "application/json")
+        };
 
-        var response = await _client.SendAsync(null);
+        var response = await _httpClient.SendAsync(request);
+
+        var responseBody = await response.Content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new InvalidOperationException(
+                $"SMSPortal failed: {(int)response.StatusCode} {responseBody}");
+        }
+
     }
 }
