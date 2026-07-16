ac64263a60eca7766daa3817184c67f549ad64e3
babychucks <babychucks05@gmail.com>
2026-07-16 20:17:41 +0200

    Made progress bar for activation page

--- web/src/app/dev-playground/page.tsx
+++ web/src/app/dev-playground/page.tsx
@@ -1,14 +1,17 @@
 import { ActivationInfoItem } from '@/components/molecules/activation-info-item/activation-info-item'
+import { ActivationProgress } from '@/components/molecules/activation-progress-bar'
 import { ShieldCheck } from 'lucide-react'
 export default function Page() {
   return (
-    <div className=" m-10 justify-center ">
+    <div className="flex items-center m-10 justify-center ">
       <ActivationInfoItem
         key={'Security'}
         icon={ShieldCheck}
         title={'Secure & Encrypted'}
         description={'Your data is protected with bank-level security'}
       />
+
+      <ActivationProgress currentStep={1} />
     </div>
   )
 }
