86e803bcb51fbd5b20fb3c5e7657ef8624af607a
TINODIWA <nathaniel.chisadza@gmail.com>
2026-07-22 19:35:22 +0200

    Serialize InstitutionType/DisclosurePolicyMode as strings instead of raw enum ints in VerifyBadgeResponseDto

--- backend/FlashIdBackend/Application/Features/Officials/DTOs/VerifyBadgeResponseDto.cs
+++ backend/FlashIdBackend/Application/Features/Officials/DTOs/VerifyBadgeResponseDto.cs
@@ -1,11 +1,14 @@
 using Domain.Enums;
+using System.Text.Json.Serialization;
 
 namespace Application.Features.Officials.DTOs;
 
 public class VerifyBadgeResponseDto
 {
     public string InstitutionName { get; set; } = string.Empty;
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public InstitutionType InstitutionType { get; set; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public DisclosurePolicyMode Mode { get; set; }
     public List<string> SuggestedIdentityDocumentFields { get; set; } = new();
     public List<string> SuggestedDriversLicenseFields { get; set; } = new();
