18eaa355fb8caed535191f036d2209d64aae1cfb
babychucks <babychucks05@gmail.com>
2026-07-22 23:58:32 +0200

    Implemented  repository functions for get credentials.

--- government-registry/GovernmentRegistry/Application/Common/Interfaces/RepositoryInterfaces/ICredentialsRepository.cs
+++ government-registry/GovernmentRegistry/Application/Common/Interfaces/RepositoryInterfaces/ICredentialsRepository.cs
@@ -1,6 +1,10 @@
+using Domain.Entities;
+
 namespace Application.Common.Interfaces.RepositoryInterfaces;
 
 public interface ICredentialsRepository
 {
+    Task<Credential?> GetIdentityDocumentBySaIdAsync(string saId, CancellationToken cancellationToken);
+    Task<Credential?> GetDriversLicenseBySaIdAsync(string saId, CancellationToken cancellationToken);
     
 }
--- government-registry/GovernmentRegistry/Infrastructure/Repositories/CredentialsRepository.cs
+++ government-registry/GovernmentRegistry/Infrastructure/Repositories/CredentialsRepository.cs
@@ -1,8 +1,26 @@
 using Application.Common.Interfaces.RepositoryInterfaces;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories;
 
 public class CredentialsRepository:ICredentialsRepository
 {
-    
+    private readonly AppDbContext _context;
+    public CredentialsRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<Credential?> GetIdentityDocumentBySaIdAsync(string saId, CancellationToken cancellationToken)
+    {
+        return await _context.IdentityDocuments.AsNoTracking()
+            .Include(document => document.Citizen)
+            .FirstOrDefaultAsync(document => document.Citizen.SaId == saId, cancellationToken);
+    }
+    public async Task<Credential?> GetDriversLicenseBySaIdAsync(string saId, CancellationToken cancellationToken){
+        return await _context.DriversLicenses.AsNoTracking()
+            .Include(document => document.Citizen)
+            .FirstOrDefaultAsync(license => license.Citizen.SaId == saId, cancellationToken);
+    }
 }
