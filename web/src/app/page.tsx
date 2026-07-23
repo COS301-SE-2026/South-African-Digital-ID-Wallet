816f37aa9520a2e86876b6499b21e69ed85ce0ca
Dominiqu <u24580482@tuks.co.za>
2026-07-23 18:29:21 +0200

    account card, delete account card and updated index.ts files

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,7 @@
 import { LoginPage } from '@/components/pages'
+import ManageUserAccount from '@/components/pages/citizen/manage-user-account-page'
 
 export default function Home() {
-  return <LoginPage />
+  // return <LoginPage />
+  return <ManageUserAccount />
 }
