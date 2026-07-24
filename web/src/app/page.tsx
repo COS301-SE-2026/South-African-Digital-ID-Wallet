ba6de362d177403ba150b36ae2f6b1f2c035d2d7
Dominiqu <u24580482@tuks.co.za>
2026-07-24 23:30:19 +0200

    manage user acc service and repo. and fixed routing to the page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,5 @@
 import { LoginPage } from '@/components/pages'
-import ManageUserAccount from '@/components/pages/citizen/manage-user-account-page'
 
 export default function Home() {
-  // return <LoginPage />
-  return <ManageUserAccount />
+  return <LoginPage />
 }
