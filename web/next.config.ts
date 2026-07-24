1223a7cce765d9b00965489987285ece79e3a8a6
TINODIWA <nathaniel.chisadza@gmail.com>
2026-07-24 17:53:00 +0200

    Testing deployment

--- web/next.config.ts
+++ web/next.config.ts
@@ -4,14 +4,6 @@ import path from 'node:path'
 const nextConfig: NextConfig = {
   output: 'standalone',
   outputFileTracingRoot: path.join(__dirname, '../'),
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.API_INTERNAL_URL ?? 'http://localhost:5118'}/api/:path*`,
-      },
-    ]
-  },
 }
 
 export default nextConfig
