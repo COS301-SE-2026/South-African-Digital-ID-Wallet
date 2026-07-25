d1622c67c5299e01499843265dd2e02c4c40384e
Ryan-Liao-Code <u24573699@tuks.co.za>
2026-07-25 15:16:24 +0200

    feature/added-manage-user-account-service

---
+++ web/src/services/manage-user-account-service/index.ts
@@ -0,0 +1,3 @@
+export { default as manageUserAccountService } from './manage-user-account-service'
+export { default as manageUserAccountUrls } from './manage-user-account-urls'
+export * from './types'
---
+++ web/src/services/manage-user-account-service/manage-user-account-service.ts
@@ -0,0 +1,38 @@
+import api from '@/lib/api'
+import manageUserAccountUrls from './manage-user-account-urls'
+import type { ManageUserAccountDto, MessageResponse } from './types'
+
+const getMyAccount = (): Promise<ManageUserAccountDto> =>
+  api
+    .get(manageUserAccountUrls.me())
+    .then((res) => res.data as ManageUserAccountDto)
+
+const verifyPassword = (password: string): Promise<MessageResponse> =>
+  api
+    .post(manageUserAccountUrls.verifyPassword(), { password })
+    .then((res) => res.data as MessageResponse)
+
+const requestEmailChange = (newEmail: string): Promise<MessageResponse> =>
+  api
+    .post(manageUserAccountUrls.requestEmailChange(), { newEmail })
+    .then((res) => res.data as MessageResponse)
+
+const resendEmailChangeOtp = (): Promise<MessageResponse> =>
+  api
+    .post(manageUserAccountUrls.resendEmailChangeOtp())
+    .then((res) => res.data as MessageResponse)
+
+const confirmEmailChange = (otp: string): Promise<ManageUserAccountDto> =>
+  api
+    .post(manageUserAccountUrls.confirmEmailChange(), { otp })
+    .then((res) => res.data as ManageUserAccountDto)
+
+const manageUserAccountService = {
+  getMyAccount,
+  verifyPassword,
+  requestEmailChange,
+  resendEmailChangeOtp,
+  confirmEmailChange,
+}
+
+export default manageUserAccountService
