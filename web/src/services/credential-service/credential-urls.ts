098a64f2f9153ef3a2da6e92619cfed7567cfa0f
Ryan-Liao-Code <u24573699@tuks.co.za>
2026-07-27 18:53:47 +0200

    hotfix/changed-code-based-on-comments

--- web/src/services/credential-service/credential-urls.ts
+++ web/src/services/credential-service/credential-urls.ts
@@ -1,7 +1,5 @@
-const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:5118'
-
 const credentialUrls = {
-  getMine: (): string => `${apiUrl}/api/credentials/me`,
+  getMine: (): string => `/api/credentials/me`,
 }
 
 export default credentialUrls
