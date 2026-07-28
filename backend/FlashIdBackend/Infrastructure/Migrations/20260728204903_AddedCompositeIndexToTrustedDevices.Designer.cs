c4d9913165b8b975d032f81390d386ec57c878e1
babychucks <babychucks05@gmail.com>
2026-07-29 00:16:16 +0200

    Adding migrations for trusted devices entity edit.

--- backend/FlashIdBackend/Infrastructure/Migrations/20260728204903_AddedCompositeIndexToTrustedDevices.Designer.cs
+++ backend/FlashIdBackend/Infrastructure/Migrations/20260728204903_AddedCompositeIndexToTrustedDevices.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260728204903_AddedCompositeIndexToTrustedDevices")]
+    partial class AddedCompositeIndexToTrustedDevices
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -666,7 +669,6 @@ namespace Infrastructure.Migrations
                         .HasColumnType("uniqueidentifier");
 
                     b.Property<string>("Browser")
-                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<DateTime>("CreatedAt")
