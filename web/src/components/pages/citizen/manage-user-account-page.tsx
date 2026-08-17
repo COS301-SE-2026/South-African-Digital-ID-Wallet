283669200a19acd03808aa592f438537c4b05499
Dominiqu <u24580482@tuks.co.za>
2026-08-17 13:17:08 +0200

    updated styling for email,  password, acc cards and manage user acc page

--- web/src/components/pages/citizen/manage-user-account-page.tsx
+++ web/src/components/pages/citizen/manage-user-account-page.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import { ManageUserAccount } from '@/components/organisms/manage-user-account'
-import { useUser } from '@/context/user-context'
-import { manageUserAccountNavSections } from '@/config/navigation'
 
 export default function ManageUserAccountPage() {
   return <ManageUserAccount />
