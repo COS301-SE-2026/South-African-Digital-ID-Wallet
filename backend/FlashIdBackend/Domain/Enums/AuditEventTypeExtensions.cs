a6e65bd99a724346a45cad5bf14131c71048cf56
TINODIWA <nathaniel.chisadza@gmail.com>
2026-08-30 18:49:14 +0200

    Moved the id masker & renamed AuditOutcomeMapper to AuditEventTypeExtensions and moved it to Enums directory inside domain as it is reall just an extension of the AuditEventType enum

---
+++ backend/FlashIdBackend/Domain/Enums/AuditEventTypeExtensions.cs
@@ -0,0 +1,17 @@
+using Domain.Enums;
+
+namespace Doamin.Enums;
+
+public static class AuditEventTypeExtensions
+{
+    public static readonly HashSet<AuditEventType> FailedEvents = new()
+    {
+        AuditEventType.OnboardCitizenFailed,
+        AuditEventType.CitizenVerificationFailed,
+        AuditEventType.CitizenCredentialsActivationFailed,
+        AuditEventType.DeviceVerificationFailed,
+    };
+
+    public static string ToOutcome(this AuditEventType eventType) =>
+        FailedEvents.Contains(eventType) ? "Failed" : "Success";
+}
